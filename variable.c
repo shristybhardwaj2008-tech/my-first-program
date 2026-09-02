34ad92441bc9c722ad5e15a8054ae9c9aedfb021
shristybhardwaj2008-tech <shristybhardwaj2008@gmail.com>
2026-09-02 13:40:09 +0530

    Correct printf syntax and variable initialization
    Fix syntax errors in printf statements and variable declarations.

---
+++ variable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int age = 17;
+    float height = 5.5;
+    char initial = 'S';
+
+printf("my age is %d\n",age);
+printf("my height is %f\n",height0;
+printf("my initial is%c\n",initial);
+printf("I am shristy bhardwaj-BCA student\n");
+
+return 0;
+}
