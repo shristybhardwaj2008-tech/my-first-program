7941a388272ad48c2b7b000bd66393537f3bdb17
shristybhardwaj2008-tech <shristybhardwaj2008@gmail.com>
2026-09-02 14:17:24 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,12 @@
+include<stdio.h>
+int main(){
+int a,b;
+printf("enter first number:");
+scanf("%d",&a);
+printf("enter second number:");
+scanf("%d",&b);
+
+printf("sum is %d\n",a+b);
+printf("i am shristy bhardwaj-my own calculator\n");
+return 0;
+}
