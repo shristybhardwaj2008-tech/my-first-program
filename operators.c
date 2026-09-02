40584179eaa53402194be93c317d8eb754a5dbde
shristybhardwaj2008-tech <shristybhardwaj2008@gmail.com>
2026-09-02 14:04:19 +0530

    Create operators.c

---
+++ operators.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int a = 10;
+int b = 20;
+
+int sum = a+b;
+int sub = a-b;
+int mul = a*b;
+
+printf("a=%d,b=%d\n",a,b);
+printf("sum is %d\n",sum);
+printf("sub is %d\n",sub);
+printf("mul is %d\n",mul);
+printf("i am shristy bhardwaj-learning operators\n");
+return 0;
+}
